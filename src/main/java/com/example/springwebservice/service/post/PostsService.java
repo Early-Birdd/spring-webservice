f6eb2f83c7caad2bb3815246a18e985f46ab9333
EarlyBird <goqhwk1100@nate.com>
2021-11-03 20:52:06 +0900

    Commit

---
+++ src/main/java/com/example/springwebservice/service/post/PostsService.java
@@ -0,0 +1,39 @@
+package com.example.springwebservice.service.post;
+
+import com.example.springwebservice.domain.posts.Posts;
+import com.example.springwebservice.domain.posts.PostsRepository;
+import com.example.springwebservice.web.dto.PostsResponseDto;
+import com.example.springwebservice.web.dto.PostsSaveRequestDto;
+import com.example.springwebservice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor //final 선언된 필드 생성자 생성 -> Bean 주입, @Autowired는 권장되지 않음
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/com/example/springwebservice/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.springwebservice.web.dto;
+
+import com.example.springwebservice.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
