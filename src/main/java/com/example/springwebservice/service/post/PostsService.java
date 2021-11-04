226699835c17575a8037732122e4ec51da1e1028
EarlyBird <goqhwk1100@nate.com>
2021-11-04 14:34:57 +0900

    Commit

--- src/main/java/com/example/springwebservice/service/post/PostsService.java
+++ src/main/java/com/example/springwebservice/service/post/PostsService.java
@@ -2,12 +2,16 @@ package com.example.springwebservice.service.post;
 
 import com.example.springwebservice.domain.posts.Posts;
 import com.example.springwebservice.domain.posts.PostsRepository;
+import com.example.springwebservice.web.dto.PostsListResponseDto;
 import com.example.springwebservice.web.dto.PostsResponseDto;
 import com.example.springwebservice.web.dto.PostsSaveRequestDto;
 import com.example.springwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor //final 선언된 필드 생성자 생성 -> Bean 주입, @Autowired는 권장되지 않음
 @Service
@@ -35,5 +39,22 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+
+        //map(PostsListResponseDto::new) => map(posts -> new PostsListResponseDto(posts))
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
     }
 }
