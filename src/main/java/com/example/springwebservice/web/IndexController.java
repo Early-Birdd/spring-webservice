ebbdad493576bd356fb8cb4e2898d95c6a31c615
EarlyBird <goqhwk1100@nate.com>
2021-12-13 23:05:08 +0900

    Commit

--- src/main/java/com/example/springwebservice/web/IndexController.java
+++ src/main/java/com/example/springwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springwebservice.web;
 
+import com.example.springwebservice.config.auth.dto.SessionUser;
 import com.example.springwebservice.service.post.PostsService;
 import com.example.springwebservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
 
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
